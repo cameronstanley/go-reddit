04a77313af307a4485d20c7147b22f3afda11dae
Cameron Stanley <cameronstanley23@gmail.com>
2016-10-17 22:38:19 -0500

    Stop using new keyword when creating Client in tests

--- article_test.go
+++ article_test.go
@@ -12,7 +12,7 @@ func TestGetHotArticles(t *testing.T) {
 	mockResponseFromFile(url, "test_data/article/hot_articles.json")
 	defer httpmock.DeactivateAndReset()
 
-	client := new(Client)
+	client := Client{}
   articles, err := client.GetHotArticles("news")
   assert.NoError(t, err)
   assert.Equal(t, len(articles), 3)
